126c00657449f76e7773a1c8b9a2dbd74aa1d996
Konrad Wienecke <konrad.wienecke@mac.com>
2020-04-09 17:49:58 +0200

    CLI: program name can now be omitted if file only includes one program

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,13 @@ import ArbitraryInterpreter.Exec.RunProgram
 import ArbitraryInterpreter.Parse.ParseCollection
 import ArbitraryInterpreter.Defs
 import Control.Monad (when)
-import Data.Map ((!))
-import Data.Maybe (fromJust)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust, isNothing)
 import System.Console.Docopt
 import System.Environment (getArgs)
 
 -- TODO LICENSE!!!
+-- TODO test reading input from file
 
 patterns :: Docopt
 patterns = [docoptFile|USAGE.txt|]
@@ -16,18 +17,17 @@ patterns = [docoptFile|USAGE.txt|]
 getArgOrExit = getArgOrExitWith patterns
 
 main = do
-    print =<< getArgs
     args <- parseArgsOrExit patterns =<< getArgs
 
-    progText  <- readFile =<< args `getArgOrExit` (argument "FILE")
-    progName  <- args `getArgOrExit` (argument "PROGNAME")
-    progInput <- getProgInput args
-
+    progText        <- readFile =<< args `getArgOrExit` (argument "FILE")
+    progInput       <- getProgInput args
     let (moc, progs) = parseCollection progText
+    progName        <- getProgName args progs
+
     if (args `isPresent` (shortOption 't'))
-        then runPrintTrace Nothing moc (progs ! progName) progInput
+        then runPrintTrace Nothing moc (progs Map.! progName) progInput
         else do
-            let (pstate, mstate) = run Nothing moc (progs ! progName) progInput
+            let (pstate, mstate) = run Nothing moc (progs Map.! progName) progInput
             putStrLn $ pstate ++ " " ++ mstate
 
 
@@ -36,3 +36,18 @@ getProgInput args
     | args `isPresent` (shortOption 'i') = args `getArgOrExit` (shortOption 'i')
     | args `isPresent` (shortOption 'l') = readFile =<< args `getArgOrExit` (shortOption 'l')
     | otherwise = exitWithUsage patterns
+
+
+-- in case the program collection only contains one program, the program name
+-- can be omitted. Should this not be the case and the program name is still
+-- omitted, or should the program name not appear in the program map, an error
+-- is thrown.
+getProgName :: Arguments -> Map.Map ProgramName Program -> IO ProgramName
+getProgName args progs
+    | Map.size progs == 1 && isNothing progName = return . head $ Map.keys progs
+    | isNothing progName = error "bla" -- exitWithUsage patterns
+    | Map.member progName' progs = return progName'
+    | otherwise = error $ progName' ++ " is not in program collection"
+    where
+        progName  = args `getArg` (shortOption 'p')
+        progName' = fromJust progName
