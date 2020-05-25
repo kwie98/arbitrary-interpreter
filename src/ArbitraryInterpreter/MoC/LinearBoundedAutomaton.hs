891377fa24731d63c7578fdb26ea069aff4ca33c
Konrad Wienecke <konrad.wienecke@mac.com>
2020-05-25 18:43:24 +0200

    combined operations and crashing for LBA

--- src/ArbitraryInterpreter/MoC/LinearBoundedAutomaton.hs
+++ src/ArbitraryInterpreter/MoC/LinearBoundedAutomaton.hs
@@ -6,6 +6,10 @@ import ArbitraryInterpreter.Defs
 import Data.Maybe (fromJust, isNothing)
 import Text.Read (readMaybe)
 
+-- inner machine state format, a MachineState is just the "shown" version of a LBAState
+type LBAState = (String, Int, Bool)
+-- TODO test stuff with crashing and combined operations!!!
+
 -- checks the given arguments before passing them to MoC builder, precisely:
 -- number of arguments is 1
 -- arg0 is a string description of a valid TM alphabet (as checked by other function)
@@ -35,11 +39,15 @@ buildLBA alphabet = MoC (isValidLBAState alphabet) ops preds
             ('W':s:[]) -> if s `elem` alphabet
                 then Just $ apply (write s)
                 else Nothing
+            ('W':s:d:[]) -> if s `elem` alphabet && d `elem` "LNR"
+                then Just $ apply (move d . write s)
+                else Nothing
+            "CRASH" -> Just $ apply (crash)
             _ -> Nothing
 
         preds predname = case predname of
-            "LE" -> Just $ check (\(_, pos) -> pos <= (-1))
-            "RE" -> Just $ check (\(tape, pos) -> pos >= length tape)
+            "LE" -> Just $ check (\(_, pos, _) -> pos <= (-1))
+            "RE" -> Just $ check (\(tape, pos, _) -> pos >= length tape)
             ('=':s:[]) -> if s `elem` alphabet
                 then Just $ check (isSymbol s)
                 else Nothing
@@ -61,47 +69,58 @@ isValidLBAState alphabet ms
     | any (\s -> not $ s `elem` alphabet) tape =  False -- all symbols need to be valid
     | otherwise = True
     where
-        ms'  = readMaybe ms :: Maybe (String, Int)
-        (tape, pos) = fromJust ms'
+        ms'  = readMaybe ms :: Maybe LBAState
+        (tape, pos, _) = fromJust ms'
 
 
 -- reads machine state, applies operation and then returns string form of
 -- machine state again
-applyLBAOperation :: ((String, Int) -> (String, Int)) -> MachineState -> MachineState
-applyLBAOperation op ms = show $ op (read ms :: (String, Int))
+applyLBAOperation :: (LBAState -> LBAState) -> MachineState -> MachineState
+applyLBAOperation op ms = show $ op (read ms :: LBAState)
 
 
 -- reads machine state and checks predicate
-checkLBAPredicate :: ((String, Int) -> Bool) -> MachineState -> Bool
-checkLBAPredicate p ms = p (read ms :: (String, Int))
+checkLBAPredicate :: (LBAState -> Bool) -> MachineState -> Bool
+checkLBAPredicate p ms = p (read ms :: LBAState)
 
 
-isSymbol :: Char -> (String, Int) -> Bool
-isSymbol s (tape, pos)
+isSymbol :: Char -> LBAState -> Bool
+isSymbol s (tape, pos, _)
     | pos <= (-1) = False
     | pos >= length tape = False
     | otherwise = tape !! pos == s
 
 
-moveLeft :: (String, Int) -> (String, Int)
-moveLeft (tape, pos)
-    | pos <= 0 = (tape, -1) -- position is left bound
-    | otherwise = (tape, pos - 1)
+moveLeft :: LBAState -> LBAState
+moveLeft (tape, pos, crashed)
+    | pos <= 0 = (tape, -1, crashed) -- position is left bound
+    | otherwise = (tape, pos - 1, crashed)
 
 
-moveRight :: (String, Int) -> (String, Int)
-moveRight (tape, pos)
-    | pos >= r = (tape, r + 1) -- position is right bound
-    | otherwise = (tape, pos + 1)
+moveRight :: LBAState -> LBAState
+moveRight (tape, pos, crashed)
+    | pos >= r = (tape, r + 1, crashed) -- position is right bound
+    | otherwise = (tape, pos + 1, crashed)
     where
         r = length tape - 1 -- one position from right bound
 
 
-write :: Char -> (String, Int) -> (String, Int)
-write s (tape, pos)
-    | pos <= (-1) = (tape, -1)
-    | pos >= rbound = (tape, rbound)
-    | otherwise = (left ++ (s:right), pos)
+move :: Char -> LBAState -> LBAState
+move direction mstate = case direction of
+    'L' -> moveLeft mstate
+    'R' -> moveRight mstate
+    _   -> mstate -- case 'N' or any other symbol
+
+
+write :: Char -> LBAState -> LBAState
+write s (tape, pos, crashed)
+    | pos <= (-1) = (tape, -1, crashed)
+    | pos >= rbound = (tape, rbound, crashed)
+    | otherwise = (left ++ (s:right), pos, crashed)
     where
         rbound = length tape
         (left, (_:right)) = splitAt pos tape
+
+
+crash :: LBAState -> LBAState
+crash (tape, pos, _) = (tape, pos, True)
